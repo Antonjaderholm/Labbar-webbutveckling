c69d2e9f17b7c7ee23cb412f788b6d935e118544
Antonjaderholm <anton.jaderholm@iths.se>
2025-01-22 18:18:55 +0100

    neew

---
+++ menu.js
@@ -0,0 +1,54 @@
+// Menu Data Structure
+const menuItems = [
+    {
+        id: 1,
+        name: "Caviar & Blini",
+        description: "Premium Osetra caviar served with traditional garnishes",
+        price: 65,
+        image: "images/C.jpg",
+        category: "starters"
+    },
+    {
+        id: 2,
+        name: "Foie Gras Terrine",
+        description: "House-made terrine with brioche and fig jam",
+        price: 60,
+        image: "images/B.jpg",
+        category: "starters"
+    },
+    {
+        id: 3,
+        name: "Lobster Thermidor",
+        description: "Whole lobster with cognac cream sauce",
+        price: 120,
+        image: "images/BB.jpg",
+        category: "mains"
+    },
+    {
+        id: 4,
+        name: "Wagyu A5 Tenderloin",
+        description: "Japanese A5 Wagyu with truffle sauce",
+        price: 150,
+        image: "images/CC.jpg",
+        category: "mains"
+    },
+    {
+        id: 5,
+        name: "Soufflé au Chocolat",
+        description: "Grand cru chocolate soufflé with vanilla ice cream",
+        price: 45,
+        image: "images/A.jpg",
+        category: "desserts"
+    },
+    {
+        id: 6,
+        name: "Golden Chocolat",
+        description: "Golden chocolat road with vanilia cream & pistachio nuts",
+        price: 45,
+        image: "images/jj.jpg",
+        category: "desserts"
+    }
+];
+
+// Shopping Cart
+let cart = [];
